e0e189c8d440624356f47900614360d59803393a
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2024-01-17 21:01:41 +0600

    books are sorting complete

--- src/components/bookshelves/BookShelves.jsx
+++ src/components/bookshelves/BookShelves.jsx
@@ -20,6 +20,29 @@ export default function BookShelves() {
     newBooks[favIndex].isFavorite = !newBooks[favIndex].isFavorite
     setBooks(newBooks)
   }
+  const handleSortBooks = (sortOption) => {
+    function sortBooks() {
+      switch (sortOption) {
+        case 'nameAtoZ':
+          return books
+            .slice()
+            .sort((a, b) => a.book_name.localeCompare(b.book_name))
+        case 'nameZtoA':
+          return books
+            .slice()
+            .sort((a, b) => b.book_name.localeCompare(a.book_name))
+        case 'yearOldest':
+          return books.slice().sort((a, b) => a.publish_year - b.publish_year)
+        case 'yearNewest':
+          return books.slice().sort((a, b) => b.publish_year - a.publish_year)
+        default:
+          return books
+      }
+    }
+    const allSortedBooks = sortBooks()
+    setBooks([...allSortedBooks])
+  }
+
   return (
     <main className="my-10 lg:my-14">
       <header className="mx-auto mb-8 max-w-7xl lg:mb-10">
@@ -34,7 +57,7 @@ export default function BookShelves() {
             <BookSearch onSearch={handleSearchBook} />
           </div>
           {/* <!-- sort - filter --> */}
-          <SortOfBooks />
+          <SortOfBooks onSort={handleSortBooks} />
         </div>
       </header>
       <div className="container mx-auto grid max-w-7xl grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
--- src/components/bookshelves/SortOfBooks.jsx
+++ src/components/bookshelves/SortOfBooks.jsx
@@ -1,16 +1,17 @@
-export default function SortOfBooks() {
+export default function SortOfBooks({ onSort }) {
   return (
     <div className="flex items-stretch space-x-3">
       <select
+        onChange={(e) => onSort(e.target.value)}
         className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
         name="sortBy"
         id="sortBy"
       >
         <option value="">Sort</option>
-        <option value="name_asc">Name (A-Z)</option>
-        <option value="name_desc">Name (Z-A)</option>
-        <option value="year_asc">Publication Year (Oldest)</option>
-        <option value="year_desc">Publication Year (Newest)</option>
+        <option value="nameAtoZ">Name (A-Z)</option>
+        <option value="nameZtoA">Name (Z-A)</option>
+        <option value="yearOldest">Publication Year (Oldest)</option>
+        <option value="yearNewest">Publication Year (Newest)</option>
       </select>
     </div>
   )
